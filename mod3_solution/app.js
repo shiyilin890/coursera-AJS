399f403a5bb506335e08b90d502a0ecba6882d6a
Stone Croc <shiyilin890@gmail.com>
2019-11-09 22:13:05 -0700

    mod3

--- mod3_solution/app.js
+++ mod3_solution/app.js
@@ -4,7 +4,7 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
 .directive('foundItems', FoundItems);
 
 
@@ -70,4 +70,5 @@ function MenuSearchService($http, ApiBasePath) {
       });
   };
 }
+
 })();
